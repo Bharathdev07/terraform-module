99f71551c88a5a979bbd55a4b1fe54e3e97ace6b
Bharathdev07 <devops854@gmail.com>
2024-08-08 21:57:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "my-test"{
-    source="./module"
-    instance_type=var.vm
-    key_name=var.new
-    tags ={
-        name=var.stage
-    }
+module "my_test" {
+  source          = "./module"
+  instance_type   = var.vm
+  key_name         = var.new
+  tags = {
+    Name = var.stage
+  }
 }
